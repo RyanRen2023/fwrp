e6e95dafc275089f7cc31313e8359f64a31bee5f
Uyen Minh Trinh <minh.trinhuyen123@gmail.com>
2024-08-05 15:14:37 -0400

    Update FoodDao.java
    JDBC Connection + function

--- src/main/java/com/algonquin/cst8288/fwrptomc/dao/FoodDao.java
+++ src/main/java/com/algonquin/cst8288/fwrptomc/dao/FoodDao.java
@@ -4,6 +4,7 @@
  */
 package com.algonquin.cst8288.fwrptomc.dao;
 
+
 import com.algonquin.cst8288.fwrptomc.model.Food;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -22,7 +23,7 @@ public class FoodDao {
 
     public void addFood(Food food) {
         String sql = "INSERT INTO food (fname, expiration, price, inventory, discount, ftid, is_donate, store_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-
+        
         try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, food.getFname());
             pstmt.setDate(2, java.sql.Date.valueOf(food.getExpiration()));
@@ -32,7 +33,7 @@ public class FoodDao {
             pstmt.setInt(6, food.getFtid());
             pstmt.setInt(7, food.getIsDonate());
             pstmt.setInt(8, food.getStoreId());
-
+            
             pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -41,7 +42,7 @@ public class FoodDao {
 
     public void updateFood(Food food) {
         String sql = "UPDATE food SET fname = ?, expiration = ?, price = ?, inventory = ?, discount = ?, ftid = ?, is_donate = ?, store_id = ? WHERE fid = ?";
-
+        
         try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, food.getFname());
             pstmt.setDate(2, java.sql.Date.valueOf(food.getExpiration()));
@@ -52,7 +53,7 @@ public class FoodDao {
             pstmt.setInt(7, food.getIsDonate());
             pstmt.setInt(8, food.getStoreId());
             pstmt.setInt(9, food.getFid());
-
+            
             pstmt.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -61,7 +62,7 @@ public class FoodDao {
 
     public void deleteFood(int fid) {
         String sql = "DELETE FROM food WHERE fid = ?";
-
+        
         try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, fid);
             pstmt.executeUpdate();
@@ -73,11 +74,11 @@ public class FoodDao {
     public Food getFoodById(int fid) {
         String sql = "SELECT * FROM food WHERE fid = ?";
         Food food = null;
-
+        
         try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, fid);
             ResultSet rs = pstmt.executeQuery();
-
+            
             if (rs.next()) {
                 food = new Food();
                 food.setFid(rs.getInt("fid"));
@@ -93,14 +94,14 @@ public class FoodDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return food;
     }
 
     public List<Food> getAllFoods() {
         String sql = "SELECT * FROM food";
         List<Food> foods = new ArrayList<>();
-
+        
         try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql); ResultSet rs = pstmt.executeQuery()) {
             while (rs.next()) {
                 Food food = new Food();
@@ -118,44 +119,7 @@ public class FoodDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return foods;
-    }
-
-    public List<Food> getFoodsByDonatestate(boolean isDonated) {
-        String sql = "SELECT * FROM food where is_donate = '0' ";
-
-        if (isDonated) {
-            sql = "SELECT * FROM food where is_donate = '1'";
-        }
-        List<Food> foods = new ArrayList<>();
-
-        try (Connection conn = jdbcClient.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql); ResultSet rs = pstmt.executeQuery()) {
-            while (rs.next()) {
-                Food food = new Food();
-                food.setFid(rs.getInt("fid"));
-                food.setFname(rs.getString("fname"));
-                food.setExpiration(rs.getDate("expiration").toLocalDate());
-                food.setPrice(rs.getBigDecimal("price"));
-                food.setInventory(rs.getInt("inventory"));
-                food.setDiscount(rs.getDouble("discount"));
-                food.setFtid(rs.getInt("ftid"));
-                food.setIsDonate(rs.getInt("is_donate"));
-                food.setStoreId(rs.getInt("store_id"));
-                foods.add(food);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return foods;
-    }
-
-    public List<Food> getAllFoodsForDonation() {
-        return this.getFoodsByDonatestate(true);
-    }
-    
-    public List<Food> getAllFoodsForPurchase() {
-        return this.getFoodsByDonatestate(false);
     }
 }
